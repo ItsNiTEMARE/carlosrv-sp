9b45e967c684a8c294ad514c487177790c37ab8f
Carlos Rivas <rivascarlosva@gmail.com>
2021-03-17 16:59:42 -0500

    Implement React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from 'pages/HomePage';
 
 const App = (): JSX.Element => {
-    return <HomePage />;
+    return (
+        <main>
+            <Switch>
+                <Route path="/" component={HomePage} exact />
+            </Switch>
+        </main>
+    );
 };
 
 export default App;
